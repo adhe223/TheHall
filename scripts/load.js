9f771bbcf281bb844c8014c5a6603abf58c54a72
adhe223 <adhe223@g.uky.edu>
2014-10-26 11:18:30 -0500

    Fixed Many UI elements
    Ready to start the loading and aprsing of data (hard part)

--- scripts/load.js
+++ scripts/load.js
@@ -1,24 +1,33 @@
 function requestCrossDomain() {
 	var addr = document.getElementById("inputURL").value;
 
+	//Check if address was entered
 	if (!addr) {
 		alert('No site passed!');
 		return false;
 	}
 	
-	//Enable the pills
-	enablePills();
-	
 	//Set the address in local storage
 	localStorage.setItem("leagueurl", addr);
 	
-	//Add '#games-tabs1' so that only the section with the teams is returned
+	//Add '#games-tabs1' so that only the section with the teams is returned from yql query
 	var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + addr + '"') + " #games-tabs1";
-	document.getElementById("call").innerHTML = yql;
 	
-	$("#results").load(yql);
+	//New way to load and manipulate
+	
+	
+	//Old way
+	//$("#results").load(yql);
 	//$("#results").hide()		//Should find better way to do this. Just loading to manipulate later
-	
 	//Parse out items
-	var html = $("#games-tabs1").html()
+	//var html = $("#games-tabs1").html()
+	
+	//Hide the URL entry box
+	hideURL(900);
+	
+	//Enable the pills if successful
+	enablePills();
+	
+	//Enable the results boxes
+	showResults();
 }
--- scripts/page.js
+++ scripts/page.js
@@ -1,49 +1,105 @@
-$(document).ready(function() {
+$(document).ready(function() {	
+	//Set the active tab
+	localStorage.setItem("activetab", "#home-nav");
+
+	//Set up click event handlers
+	//In future refactor these into one function inside click event	
+	$("#home-nav").click(function() {
+		if (localStorage.getItem("leagueurl").length > 0) {
+			changeActiveTab(localStorage.getItem("activetab"), "#home-nav");
+		}
+	});
+	
+	$("#standingsAnchor").click(function() {
+		if (localStorage.getItem("leagueurl").length > 0) {
+			changeActiveTab(localStorage.getItem("activetab"), "#standings-nav");
+			scrollTo("#standings-box");
+		}
+	});
+	
+	$("#seasonsAnchor").click(function() {
+		if (localStorage.getItem("leagueurl").length > 0) {
+			changeActiveTab(localStorage.getItem("activetab"), "#seasons-nav");
+			scrollTo("#seasons-box");
+		}
+	});
+	
+	$("#advancedAnchor").click(function() {
+		if (localStorage.getItem("leagueurl").length > 0) {
+			changeActiveTab(localStorage.getItem("activetab"), "#advanced-nav");
+			scrollTo("#advanced-box");
+		}
+	});
+	
 	//Disable UI tabs if address is not set
 	if (localStorage.getItem("leagueurl").length == 0) {
-		//Change appearance of pills
-		$("#standings").addClass("disabled");
-		$("#seasons").addClass("disabled");
-		$("#advanced").addClass("disabled");
-		
-		//Change functionality of links
-		document.getElementById("standingsAnchor").href="javascript:";
-		document.getElementById("seasonsAnchor").href="javascript:";
-		document.getElementById("advancedAnchor").href="javascript:";
+		clearSite();
+	} else {
+		hideURL(0);
 	}
 });
 
+function changeActiveTab(oldTabID, newTabID) {
+	$(oldTabID).removeClass("active");
+	$(newTabID).addClass("active");
+	
+	localStorage.setItem("activetab", newTabID);
+}
+
 //If no site is selected, do this
 function disablePills() {
-	$("#standings").addClass("disabled");
-	$("#seasons").addClass("disabled");
-	$("#advanced").addClass("disabled");
+	$("#standings-nav").addClass("disabled");
+	$("#seasons-nav").addClass("disabled");
+	$("#advanced-nav").addClass("disabled");
 	
 	//Change functionality of links
 	document.getElementById("standingsAnchor").href="javascript:";
 	document.getElementById("seasonsAnchor").href="javascript:";
 	document.getElementById("advancedAnchor").href="javascript:";
-	
-	//Show the url entry box
-	$("#center-box").show(900);
 }
 
 //If site is selected, do this
 function enablePills() {
-	$("#standings").removeClass("disabled");
-	$("#seasons").removeClass("disabled");
-	$("#advanced").removeClass("disabled");
+	$("#standings-nav").removeClass("disabled");
+	$("#seasons-nav").removeClass("disabled");
+	$("#advanced-nav").removeClass("disabled");
 	
 	//Change functionality of links
-	document.getElementById("standingsAnchor").href="/TheHall/standings.html";
-	document.getElementById("seasonsAnchor").href="/TheHall/seasons.html";
-	document.getElementById("advancedAnchor").href="/TheHall/advanced.html";
-	
-	//Hide the url entry screen
-	$("#center-box").hide(900);
+	document.getElementById("standingsAnchor").href="#";
+	document.getElementById("seasonsAnchor").href="#";
+	document.getElementById("advancedAnchor").href="#";
+}
+
+function hideResults() {
+	$("#standings-box").hide();
+	$("#seasons-box").hide();
+	$("#advanced-box").hide();
+}
+
+function showResults() {
+	$("#standings-box").show(700);
+	$("#seasons-box").show(700);
+	$("#advanced-box").show(700);
+}
+
+function hideURL(time) {
+	$("#center-box").hide(time);
+}
+
+function showURL(time) {
+	$("#center-box").show(time);
+}
+
+function scrollTo(elementScroll) {
+	$('html, body').animate({
+		scrollTop: $(elementScroll).offset().top
+	}, 1000);
 }
 
 function clearSite() {
 	localStorage.setItem("leagueurl", "");
+	changeActiveTab(localStorage.getItem("activetab"), "#home-nav");
+	hideResults();
 	disablePills();
+	showURL(900);
 }
